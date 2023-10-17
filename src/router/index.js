a5fc99d81e6c1850123bbdb0f1c74def95d1219b
KJSOFT\lanny.syu <oopsjohn.77@gmail.com>
2023-10-17 10:56:49 +0800

    create_test_builder.io

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/product",
+    name: "product",
+    // route level code-splitting
+    // this generates a separate chunk (product.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "product" */ "../views/ProductView.vue"),
+  },
 ];
 
 const router = createRouter({
